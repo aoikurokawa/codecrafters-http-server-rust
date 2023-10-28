5e45d88a8b042e1d2652904737a8363458c29e17
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-28 15:03:34 +0900

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::{self, Read, Write},
+    net::TcpListener,
+};
 
-fn main() {
+fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    loop {
+        let (mut socket, _) = listener.accept().unwrap();
+
+        let mut buf = [0; 1024];
+        socket.read(&mut buf)?;
+        socket.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+        // for stream in listener.incoming() {
+        //     match stream {
+        //         Ok(_stream) => {
+        //             println!("accepted new connection");
+        //         }
+        //         Err(e) => {
+        //             println!("error: {}", e);
+        //         }
+        //     }
+        // }
     }
 }
