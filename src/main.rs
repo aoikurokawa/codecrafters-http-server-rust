f8cbf2cf728f8648e535b36d8bc5f258d768ec4d
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-29 08:06:59 +0900

    feat: get user agent

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ use std::{
 use itertools::Itertools;
 
 enum Path {
-    Index,
     Echo,
+    UserAgent,
     NotFound,
 }
 
 impl From<&str> for Path {
     fn from(value: &str) -> Self {
         match value {
-            "" => Self::Index,
             "echo" => Self::Echo,
+            "user-agent" => Self::UserAgent,
             _ => Self::NotFound,
         }
     }
@@ -40,16 +40,32 @@ fn main() -> io::Result<()> {
                         let res = if path == "/" {
                             "HTTP/1.1 200 OK\r\n\r\n".to_string()
                         } else {
-                            if children[1] == "echo" {
-                                let content = children.iter().skip(2).join("/");
-                                format!(
-                                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", 
-                                    content.len(), 
-                                    content
-                                )
-                            } else {
-                                "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                           match Path::from(children[1]) {
+                                Path::Echo => {
+                                    let content = children.iter().skip(2).join("/");
+                                    format!(
+                                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", 
+                                        content.len(), 
+                                        content
+                                    )
+                                },
+                                Path::UserAgent => {
+                                    let user_agent_txt = extract_user_agent(&request).unwrap();
+                                    format!(
+                                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", 
+                                        user_agent_txt.len(), 
+                                        user_agent_txt
+                                    )
+                                }
+                                Path::NotFound => {
+                                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                                }
                             }
+                            // if children[1] == "echo" {
+
+                            // } else {
+                            //     "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                            // }
                         };
 
                         socket.write(res.as_bytes())?
@@ -65,6 +81,18 @@ fn main() -> io::Result<()> {
 fn extract_path(req: &str) -> Option<&str> {
     for (idx, line) in req.lines().enumerate() {
         if idx == 0 {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+
+            return parts.get(1).copied();
+        }
+    }
+
+    None
+}
+
+fn extract_user_agent(req: &str) -> Option<&str> {
+    for (idx, line) in req.lines().enumerate() {
+        if idx == 3 {
             let parts: Vec<&str> = line.split_whitespace().collect();
 
             return parts.get(1).copied();
